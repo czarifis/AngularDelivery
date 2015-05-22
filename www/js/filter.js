6c01c930c04ba354dd7e409d896f5d6a72fc1869
czarifis <zarifis@cs.ucsd.edu>
2015-05-21 17:36:59 -0700

    cleaning up the controller

--- www/js/filter.js
+++ www/js/filter.js
@@ -2,34 +2,27 @@
  * Created by Costas Zarifis on 11/6/14.
  */
 
-Array.prototype.max = function() {
+Array.prototype.max = function () {
     return Math.max.apply(null, this);
 };
 
-Array.prototype.min = function() {
+Array.prototype.min = function () {
     return Math.min.apply(null, this);
 };
-angular.module('phonecatFilters', []).filter('checkmark', function() {
-    return function(input,scope) {
+angular.module('phonecatFilters', []).filter('checkmark', function () {
+    return function (input, scope) {
 
-
-//        scope.$watchCollection('ranges', function(newNames, oldNames) {
-
-//            console.log(input,scope.ranges);
-
-            rangeListMax=[];
-            rangeListMin=[];
-            for(var k=0;k<scope.ranges.length;k++){
-                if(scope.ranges[k].checked){
-                    rangeListMax.push(scope.ranges[k].max);
-                    rangeListMin.push(scope.ranges[k].min);
-                }
-                console.log('max:',Math.max.apply(null, rangeListMax));
-                console.log('min:',Math.min.apply(null, rangeListMin));
+        rangeListMax = [];
+        rangeListMin = [];
+        for (var k = 0; k < scope.ranges.length; k++) {
+            if (scope.ranges[k].checked) {
+                rangeListMax.push(scope.ranges[k].max);
+                rangeListMin.push(scope.ranges[k].min);
             }
-            return input;
-//        });
-
+            console.log('max:', Math.max.apply(null, rangeListMax));
+            console.log('min:', Math.min.apply(null, rangeListMin));
+        }
+        return input;
 
     };
 });
